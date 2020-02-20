a518363146d5f2df87ca1952b8f96e67e0fe1834
luyangming <2603505699@qq.com>
2020-02-20 16:08:10 +0800

    webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,8 +1,9 @@
 import img from './img/24.jpg';
 // import style from './css/index.css';
 import style from './css/test.scss';
-console.log('111');
-
+console.log('22');
+let aaaaa=100;
+console.log(aaaaa);
 
 var oImg=new Image();
 oImg.src=img;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,16 +1,20 @@
 const path = require("path");
 const HtmlWebpackPlugin=require("html-webpack-plugin");
+const {CleanWebpackPlugin}=require("clean-webpack-plugin");
+const output = require("./webpack.output.js");
+const webpack=require('webpack');
 // const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     mode:"development",
     // 入口文件 
     entry: "./src/index.js",
     // 出口文件 打包到哪
-    output: {
-        filename: "bundle.js",
-        // path是绝对路径 
-        path: path.resolve(__dirname, "dist")
-    },
+    output:output,
+    // output: {
+    //     filename: "bundle.js",
+    //     // path是绝对路径 
+    //     path: path.resolve(__dirname, "dist")
+    // },
     module: {
         rules: [
             {
@@ -40,11 +44,26 @@ module.exports = {
                     "sass-loader"
                 ]
             },
+             {
+                  test:/\.js$/,
+                  loader:"babel-loader",
+                  options:{
+                    presets:["@babel/preset-env"]
+                  },
+                  exclude: /node_modules/
+             }
         ]
     },
     plugins:[
         new HtmlWebpackPlugin({
             template:"./src/index.html"
-        })
-    ]
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.BannerPlugin("唯创所有")
+    ],
+    devServer:{
+        contentBase:path.resolve(__dirname,"dist"),
+        host:'localhost',
+        // open:true
+    }
 }
